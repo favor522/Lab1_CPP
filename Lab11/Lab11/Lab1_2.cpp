210c25f7ad85314a876790d9ccd013c81f810615
favor522 <gurin7772@yandex.com>
2024-02-12 09:16:22 +0300

    CPP

--- Lab11/Lab11/Lab1_2.cpp
+++ Lab11/Lab11/Lab1_2.cpp
@@ -10,13 +10,13 @@ if (cin.fail()) {
 	cout << "Error type!";
 	return 0;
 }
-if (c >= -3) {
+if (c <= -3) {
 	f = pow(log((c,2)+1),3);
 }
-else if (c  3) {
+else if (c > 3) {
 	f = atan(1/ 2 * c + 1);
 }
-else if (c < -3) {
+else{
 	cout << "Your number is not equal in description, enter your number again";
 	return 0;
 }
